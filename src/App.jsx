1b4442f9b968e3ad2c1437531c620da7a40ed11c
iilloveffree <iilloveffree@naver.com>
2025-07-25 17:15:30 +0900

    chore: App.jsx 라우팅에 설문조사 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import Home from './pages/Home';
+
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Test from "./pages/Test";
+import Questions from "./pages/Survey/Questions";
+import Summary from "./pages/Survey/Summary";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/test" element={<Test />} />
+      <Route path="/survey/questions" element={<Questions />} />
+      <Route path="/survey/summary" element={<Summary />} />
+    </Routes>
   );
 }
 
